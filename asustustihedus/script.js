121328c216ebb4d5e1f8737119001ac64669e739
hvirro <34108918+hvirro@users.noreply.github.com>
2017-12-02 23:29:59 +0200

    Update script.js

--- asustustihedus/script.js
+++ asustustihedus/script.js
@@ -1,6 +1,6 @@
 var map = new L.map('map');
 
 // var myLayer = new L.geoJson(ov);
-var myLayer = L.geoJson.ajax("ov.geojson");
+var myLayer = new L.geoJson.ajax("ov.geojson");
 myLayer.addTo(map);
 map.fitBounds(myLayer.getBounds());
