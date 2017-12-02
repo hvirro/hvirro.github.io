42d6045a78b00f67bd7e13fcfabd86a1b0727619
hvirro <34108918+hvirro@users.noreply.github.com>
2017-12-02 16:12:30 +0200

    Update script.js

--- asustustihedus/script.js
+++ asustustihedus/script.js
@@ -1,14 +1,9 @@
-var myLayer= L.geoJson(ov);
-var theLayers = new L.layerGroup([myLayer]);
 var map = new L.map('map', {
 layers:[myLayer],
 });
+
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-var baseLayers={};
-var overLay={
-"Omavalitsused": myLayer,
-}
-L.control.layers(baseLayers, overLay).addTo(map);
-map.fitBounds(myLayer.getBounds());
+
+var myLayer= L.geoJson(ov).addTo(map);
