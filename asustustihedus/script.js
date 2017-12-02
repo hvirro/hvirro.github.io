42be3e1c4238a9f0f283d2cf57b3e4ad9fc5affb
hvirro <34108918+hvirro@users.noreply.github.com>
2017-12-02 13:40:39 +0200

    Update script.js

--- asustustihedus/script.js
+++ asustustihedus/script.js
@@ -1,1 +1,12 @@
 var myLayer = L.geoJson(ov);
+
+map = new L.map('map', {
+	layers:[myLayer]
+});
+
+var theLayers = new L.LayerGroup([myLayer]);
+var baseLayers = { };
+var overlays = {
+	"Omavalitsused": myLayer
+};
+L.control.layers(baseLayers, overlays).addTo(map);
