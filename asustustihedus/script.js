1b9d81fc7739e054eb5335ea8dd51d50cc5c1933
hvirro <34108918+hvirro@users.noreply.github.com>
2017-12-02 13:51:07 +0200

    Update script.js

--- asustustihedus/script.js
+++ asustustihedus/script.js
@@ -13,4 +13,4 @@ L.control.layers(baseLayers, overlays).addTo(map);
 
 map.fitBounds(myLayer.getBounds());
 
-L.control.scale({imperial: false, maxWidth: 250}).addTo(map);
+L.control.scale({imperial:false, maxWidth:250}).addTo(map);
