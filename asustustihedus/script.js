ffbaa71ad8cf296c7f9701dfc0b2d0a97bd99f99
hvirro <34108918+hvirro@users.noreply.github.com>
2017-12-02 23:32:41 +0200

    Update script.js

--- asustustihedus/script.js
+++ asustustihedus/script.js
@@ -4,3 +4,5 @@ var map = new L.map('map');
 var myLayer = new L.geoJson.ajax("ov.geojson");
 myLayer.addTo(map);
 map.fitBounds(myLayer.getBounds());
+
+L.control.scale({imperial:false, maxWidth:250}).addTo(map);
