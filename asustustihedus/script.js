4c510d48ab007e32c243e66abf42212fcf0d67eb
hvirro <34108918+hvirro@users.noreply.github.com>
2017-12-02 16:17:53 +0200

    Update script.js

--- asustustihedus/script.js
+++ asustustihedus/script.js
@@ -1,6 +1,4 @@
-var map = new L.map('map', {
-layers:[myLayer],
-});
+var map = L.map('map').setView([18, 28], 4);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
