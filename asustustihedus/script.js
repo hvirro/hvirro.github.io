e990554b6eb1ead50983d4e899122c39a28a9d51
hvirro <34108918+hvirro@users.noreply.github.com>
2017-12-02 15:06:52 +0200

    Update script.js

--- asustustihedus/script.js
+++ asustustihedus/script.js
@@ -2,13 +2,6 @@ var map = L.map('map');
 
 var myLayer = L.geoJson(ov).addTo(map);
 
-var theLayers = L.LayerGroup([myLayer]);
-var baseLayers = { };
-var overlays = {
-	"Omavalitsused": myLayer
-};
-L.control.layers(baseLayers, overlays).addTo(map);
-
 map.fitBounds(myLayer.getBounds());
 
 L.control.scale({imperial:false, maxWidth:250}).addTo(map);
