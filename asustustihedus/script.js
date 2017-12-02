2459a2fc700210471359479af263942ec2f9b81a
hvirro <34108918+hvirro@users.noreply.github.com>
2017-12-02 16:05:31 +0200

    Update script.js

--- asustustihedus/script.js
+++ asustustihedus/script.js
@@ -3,6 +3,9 @@ var theLayers = new L.layerGroup([myLayer]);
 var map = new L.map('map', {
 layers:[myLayer],
 });
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 var baseLayers={};
 var overLay={
 "Omavalitsused": myLayer,
